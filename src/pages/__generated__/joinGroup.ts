e150bce038e9146422b1ce93c961cf85993117a4
Vitaly Turovsky <vital2580@icloud.com>
2020-09-30 20:53:40 +0300

    feat: add generated graphql

---
+++ src/pages/__generated__/joinGroup.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: joinGroup
+// ====================================================
+
+export interface joinGroup {
+  joinGroup: boolean;
+}
+
+export interface joinGroupVariables {
+  inviteToken: string;
+}
